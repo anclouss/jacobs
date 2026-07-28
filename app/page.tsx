22d2a3106e95bb621d22396a1a32fb60a69404d1
anclouss <mlg360noscopehomyak@gmail.com>
2026-07-28 19:03:55 +0300

    Delete app directory

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import JacobsExchangeApp from "@/components/jacobs-exchange-app";
-
-export const dynamic = "force-dynamic";
-
-export default function HomePage() {
-  return <JacobsExchangeApp />;
-}
